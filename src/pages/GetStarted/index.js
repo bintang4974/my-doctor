d61a53b9abf049f2e0275a63a8a80b8b952b6e55
bintang4974 <bintangramadhan4974@gmail.com>
2021-04-27 14:28:01 +0700

    menambahkan react navigation

---
+++ src/pages/GetStarted/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { ImageBackground, StyleSheet, Text, View } from 'react-native'
+import { ILGetStarted, ILLogo } from '../../assets'
+import { Button } from '../../components'
+
+const GetStarted = ({navigation}) => {
+    return (
+        <ImageBackground source={ILGetStarted} style={styles.page}>
+            <View>
+                <ILLogo />
+                <Text style={styles.title}>Konsultasi dengan dokter jadi lebih mudah & fleksibel</Text>
+            </View>
+            <View>
+                <Button title="Get Started" onPress={() => navigation.navigate('Register')} />
+                <View style={{height: 16}} />
+                <Button type="secondary" title="Sign In" onPress={() => navigation.replace('Login')} />
+            </View>
+        </ImageBackground>
+    )
+}
+
+export default GetStarted
+
+const styles = StyleSheet.create({
+    page: {
+        padding: 40,
+        justifyContent: 'space-between',
+        backgroundColor: 'white',
+        flex: 1
+    },
+    title: {
+        fontSize: 28,
+        color: 'white',
+        marginTop: 91,
+        fontFamily: 'Nunito-SemiBold'
+    }
+})

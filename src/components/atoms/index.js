41bb21692226a0f6e47c0336f789090c6a0be4d7
bintang4974 <bintangramadhan4974@gmail.com>
2021-04-28 15:43:32 +0700

    menambahkan bottom navigator, dan membuat menjadi dinamis

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -2,5 +2,6 @@ import Button from './Button';
 import Input from './Input';
 import Gap from './Gap';
 import Link from './Link';
+import TabItem from './TabItem';
 
-export { Button, Input, Gap, Link };
+export { Button, Input, Gap, Link, TabItem };

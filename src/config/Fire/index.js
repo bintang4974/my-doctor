6fef9743d4cd311736bf3523716273224a5c7a72
bintang4974 <bintangramadhan4974@gmail.com>
2021-05-24 17:20:56 +0700

    adding firebase

---
+++ src/config/Fire/index.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyD5XDlL37dKtckahruJRwkM_WGDV8MNvuA",
+    authDomain: "my-doctor-204a5.firebaseapp.com",
+    projectId: "my-doctor-204a5",
+    storageBucket: "my-doctor-204a5.appspot.com",
+    messagingSenderId: "537906939818",
+    appId: "1:537906939818:web:69af4865fdd2da07098dd3"
+})
+
+const Fire = firebase;
+export default Fire;

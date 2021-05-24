742831259f854e1ce3f57d207ca262517be61c05
bintang4974 <bintangramadhan4974@gmail.com>
2021-05-24 21:49:07 +0700

    error message

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -3,6 +3,7 @@ import { ScrollView, StyleSheet, View } from 'react-native';
 import { Button, Gap, Header, Input, Loading } from '../../components';
 import { colors, useForm } from '../../utils';
 import { Fire } from '../../config';
+import { showMessage, hideMessage } from 'react-native-flash-message';
 
 const Register = ({ navigation }) => {
     const [form, setForm] = useForm({
@@ -16,6 +17,8 @@ const Register = ({ navigation }) => {
 
     const onContinue = () => {
         console.log(form);
+
+        
         setLoading(true)
         Fire.auth().createUserWithEmailAndPassword(form.email, form.password)
             .then((success) => {
@@ -26,7 +29,13 @@ const Register = ({ navigation }) => {
             .catch((error) => {
                 const errorMessage = error.message;
                 setLoading(false);
-                console.log('error register: ', errorMessage);
+                showMessage({
+                    message: errorMessage,
+                    type: 'default',
+                    backgroundColor: colors.error,
+                    color: colors.white
+                });
+                console.log('error: ', errorMessage);
             });
         // navigation.navigate('UploadPhoto');
     }

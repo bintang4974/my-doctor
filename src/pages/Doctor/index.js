de9551cd652400a7356bd08b2c549300d4041447
bintang4974 <bintangramadhan4974@gmail.com>
2021-06-02 17:13:33 +0700

    adding firebase storage & data dynamic news item

--- src/pages/Doctor/index.js
+++ src/pages/Doctor/index.js
@@ -1,10 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { DoctorCategory, Gap, HomeProfile, NewsItem, RatedDoctor } from '../../components';
-import { colors, fonts, getData } from '../../utils';
+import { colors, fonts, getData, showError } from '../../utils';
 import { DummyDoctor1, DummyDoctor2, DummyDoctor3, JSONCategoryDoctor } from '../../assets';
+import { Fire } from '../../config';
 
 const Doctor = ({ navigation }) => {
+    const [news, setNews] = useState([]);
+
+    useEffect(() => {
+        Fire.database()
+            .ref('news/')
+            .once('value')
+            .then((res) => {
+                console.log('data: ', res.val());
+                if (res.val()) {
+                    setNews(res.val());
+                }
+            }).catch((err) => {
+                showError(err.message);
+            });
+    }, []);
+
     return (
         <View style={styles.page}>
             <View style={styles.content}>
@@ -52,9 +69,16 @@ const Doctor = ({ navigation }) => {
                         />
                         <Text style={styles.sectionLabel}>Good News</Text>
                     </View>
-                    <NewsItem />
-                    <NewsItem />
-                    <NewsItem />
+                    {news.map((item) => {
+                        return (
+                            <NewsItem
+                                key={item.id}
+                                title={item.title}
+                                date={item.date}
+                                image={item.image}
+                            />
+                        )
+                    })}
                     <Gap height={30} />
                 </ScrollView>
             </View>

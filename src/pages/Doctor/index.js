fdefe2b4b6d421d7e011a9ddfef2282d216a7e21
bintang4974 <bintangramadhan4974@gmail.com>
2021-06-08 13:58:19 +0700

    remove warning filter empty array

--- src/pages/Doctor/index.js
+++ src/pages/Doctor/index.js
@@ -20,9 +20,10 @@ const Doctor = ({ navigation }) => {
             .ref('category_doctor/')
             .once('value')
             .then((res) => {
-                console.log('category doctor: ', res.val());
                 if (res.val()) {
-                    setCategoryDoctor(res.val());
+                    const data = res.val();
+                    const filterData = data.filter(el => el !== null);
+                    setCategoryDoctor(filterData);
                 }
             }).catch((err) => {
                 showError(err.message);
@@ -57,9 +58,11 @@ const Doctor = ({ navigation }) => {
             .ref('news/')
             .once('value')
             .then((res) => {
-                console.log('data: ', res.val());
                 if (res.val()) {
-                    setNews(res.val());
+                    const data = res.val();
+                    const filterData = data.filter(el => el !== null);
+                    console.log('data news filter: ', filterData);
+                    setNews(filterData);
                 }
             }).catch((err) => {
                 showError(err.message);
@@ -81,7 +84,7 @@ const Doctor = ({ navigation }) => {
                                 <Gap width={32} />
                                 {categoryDoctor.map(item => {
                                     return <DoctorCategory
-                                        key={item.id}
+                                        key={`category-${item.id}`}
                                         category={item.category}
                                         onPress={() => navigation.navigate('ChooseDoctor', item)} />
                                 })}
